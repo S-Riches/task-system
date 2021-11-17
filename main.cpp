f4cba3651e5c0f91ca9dc9667a9b9814a5de3a36
Sam R <samuelricheswork@gmail.com>
2021-11-17 02:30:22 +0000

    'finished' main

--- main.cpp
+++ main.cpp
@@ -34,16 +34,24 @@ string dataValidation(string input)
 }
 */
   
-//vector to hold the other vectors
-vector<vector<string>> holder;
+// appologies in advance for this sacrilege
+vector<string> group1;
+vector<string> group2;
+vector<string> group3;
+vector<string> group4;
+vector<string> group5;
+vector<string> group6;
+vector<string> group7;
+vector<string> group8;
+vector<string> group9;
+vector<string> group10;
+vector<vector<string>> holder = {group1,group2,group3,group4,group5,group6,group7,group8,group9,group10};
 //get input from txt file, put into string vector or array, return the value to then be 
 void getInput(string fileLoc)
 {
     // temp variables for comparison
     string tempStr;
     string tempNum;
-    // temp vector to add to the holder
-    vector <string> col;
 
     // instantiate an ifstream object to use to read the file
     fstream fileStream(fileLoc, ios::in);
@@ -51,7 +59,7 @@ void getInput(string fileLoc)
         if(fileStream.is_open())
         {
             // text to confirm the file was opened succesfully
-            cout << "file opened";
+            cout << "file opened\n";
             while (getline(fileStream, tempStr))
             {
                 // get the first two characters from tempstr and then store them in a temp string for processing.
@@ -61,46 +69,70 @@ void getInput(string fileLoc)
                 // ugly switch statement, as they normally are :(
                 switch (stoi(tempNum))
                 {
+                    // temporary solution for a problem, how do we scale up this without using a case switch like this?? its a bit spaghetti like
                 case 1:
-                    /* code */
+                    holder.at(0).push_back(tempStr);
+                    cout << "group 1\n";
                     break;
                 case 2:
-                    /* code */
+                    holder.at(1).push_back(tempStr);
+                    cout << "group 2\n";
                     break;
                 case 3:
-                    /* code */
+                    holder.at(2).push_back(tempStr);
+                    cout << "group 3\n";
                     break;
                 case 4:
-                    /* code */
+                    holder.at(3).push_back(tempStr);
+                    cout << "group 4\n";
                     break;
                 case 5:
-                    /* code */
+                    holder.at(4).push_back(tempStr);
+                    cout << "group 5\n";
                     break;
                 case 6:
-                    /* code */
+                    holder.at(5).push_back(tempStr);
+                    cout << "group 6\n";
                     break;
                 case 7:
-                    /* code */
+                    holder.at(6).push_back(tempStr);
+                    cout << "group 7\n";
                     break;
                 case 8:
-                    /* code */
+                    holder.at(7).push_back(tempStr);
+                    cout << "group 8\n";
                     break;
                 case 9:
-                    /* code */
+                    holder.at(8).push_back(tempStr);
+                    cout << "group 9\n";
                     break;
                 case 10:
-                    /* code */
+                    holder.at(9).push_back(tempStr);
+                    cout << "group 10\n";
                     break;
-                
                 default:
+                    cout << "error occured!" << endl;
                     break;
+
                 }
+                cout << tempStr << endl;
             }
         }
     fileStream.close();
-    // call out inv
-    // close file
+}
 
+void outputGroups()
+{
+    for(int i = 0; i < holder.size(); i++)
+    {
+        cout << "\n--Group " + to_string(i+1) + "--\n";
+        for(int x = 0; x < holder[i].size(); x++){
+            
+            cout << holder[i][x] << ", ";
+            
+        }
+
+    }
 }
 
 int main()
@@ -114,7 +146,9 @@ int main()
     cin >> fileLoc;
     //dataValidation(fileLoc);
     getInput(fileLoc);
-
+    cout << "---------------------" << endl;
+    outputGroups();
+    
 
     return 0;
 }
